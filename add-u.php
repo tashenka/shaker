3cf0ceffd093833c02757a86aac7f2e65f0b5976
Natali <tashenka-605@mail.ru>
2013-05-22 16:55:48 +0400

    Update add-u.php

--- add-u.php
+++ add-u.php
@@ -1,7 +1,7 @@
 <?php
   session_start();
   if($_GET['button']=='Добавить'){
-    $query="INSERT INTO resourses SET name='".mysql_escape_string($_GET['name'])."', position='".mysql_escape_string($_GET['position'])."', card_id='".$_GET['card_id']."', updated_at='".date("Y-m-d H:i:s")."'";
+    $query="INSERT INTO operator SET name='".mysql_escape_string($_GET['name'])."', position='".mysql_escape_string($_GET['position'])."', card_id='".$_GET['card_id']."', updated_at='".date("Y-m-d H:i:s")."'";
     mysql_query($query) or die("Err: query");
     header("Location: database.php"); exit;
   }
