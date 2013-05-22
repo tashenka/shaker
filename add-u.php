f6f448efde41dfc3af372261ae46e4ec05a9256a
Natali <tashenka-605@mail.ru>
2013-05-22 16:55:01 +0400

    Create add-u.php

---
+++ add-u.php
@@ -0,0 +1,8 @@
+<?php
+  session_start();
+  if($_GET['button']=='Добавить'){
+    $query="INSERT INTO resourses SET name='".mysql_escape_string($_GET['name'])."', position='".mysql_escape_string($_GET['position'])."', card_id='".$_GET['card_id']."', updated_at='".date("Y-m-d H:i:s")."'";
+    mysql_query($query) or die("Err: query");
+    header("Location: database.php"); exit;
+  }
+?>
