63ebeb23ff2259c62472f825d6c796f87893f023
Natali <tashenka-605@mail.ru>
2013-05-22 16:00:06 +0400

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
   $mysql = mysql_connect('localhost', 'root', 'root');
   mysql_query("SET NAMES utf8");
   $db = mysql_select_db('operator', $mysql);
-  header("Content-Type: text/html; charset=UTF-8");
+  //header("Content-Type: text/html; charset=UTF-8");
 ?>
 <head>
   <link href="/css/bootstrap.css" rel="stylesheet">
