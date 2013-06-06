f4ef7537bdda11b029087cc1edc9882a4a3445ea
Natali <tashenka-605@mail.ru>
2013-06-06 13:45:40 +0400

    Update green-zone.php

--- green-zone.php
+++ green-zone.php
@@ -2,4 +2,29 @@
 include 'header.php';
 include 'config.php';
 ?>
-Заготовка под зеленую зону
+<div class="hero-unit">
+  <h1 align="center">Зеленая зона</h1>
+  <table width="100%" border="2">
+  <tr>
+  <td width="50%">
+	    <h2 align="center">Схема порта:<h2>
+		<img src="images/port.jpg" style="width:100%" class="img-rounded">
+	</td>
+	<td>
+	  <h2 align="center">Данные о зоне:</h2>
+	</td>
+  </tr>
+  <tr>
+	<td>
+	  <h2 align="center">Аппаратные составляющие:</h2>
+		
+	</td>
+	<td>
+	  <h2 align="center">Данные о сотрудниках</h2>
+	  <p>Имеют право доступа: <?php echo $user['']; ?></p>
+	  <p>Находятся: <?php echo $user['']; ?></p>
+	</td>
+  </tr>
+  <table>
+</div>
+</html>
