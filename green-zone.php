fde66b8db2a7dc85fb156b2460696cb4930d50f4
Dmitry Gruzd <donotsendhere@gmail.com>
2013-06-07 08:08:44 +0400

    Fix green yellow

--- green-zone.php
+++ green-zone.php
@@ -1,6 +1,8 @@
 <?php
 include 'header.php';
 include 'config.php';
+$users = mysql_query("SELECT DISTINCT users.name FROM journal INNER JOIN users WHERE journal.user_id = users.id AND journal.zone = 'green' and journal.allowed = 1 ORDER BY time DESC", $mysql);
+$users_access = mysql_query("SELECT * FROM users WHERE zone_green = 1", $mysql);
 ?>
 <div class="hero-unit">
   <h1 align="center">Зеленая зона</h1>
@@ -13,8 +15,16 @@ include 'config.php';
 	<td rowspan="2">
 	  <h2 align="center">Данные о зоне:</h2>
 	    <h3 align="center">Информация о сотрудниках</h3>
-	  <p>Имеют право доступа: <?php echo $user['']; ?></p>
+	  <p>Имеют право доступа:</p>
+  <?php while ($row_y = mysql_fetch_assoc($users_access)): ?>
+<li><?php echo $row_y['name']; ?></li>
+  <?php endwhile ?>
 	  <p>Находятся: <?php echo $user['']; ?></p>
+    <ul>
+  <?php while ($row_y = mysql_fetch_assoc($users)): ?>
+<li><?php echo $row_y['name']; ?></li>
+  <?php endwhile ?>
+    </ul>
 	</td>
   </tr>
   <tr>
