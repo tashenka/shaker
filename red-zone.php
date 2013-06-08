acaaa4ca42be40ea9fca1afce00418ebbc11d0e3
Natali <tashenka-605@mail.ru>
2013-06-08 15:22:59 +0400

    Update red-zone.php

--- red-zone.php
+++ red-zone.php
@@ -3,8 +3,6 @@ include 'header.php';
 include 'config.php';
 
 $area = $_GET['area'];
-$users = mysql_query("SELECT DISTINCT users.name FROM journal INNER JOIN users WHERE journal.user_id = users.id AND journal.zone = 'red' and journal.allowed = 1 and users.deleted = 0 ORDER BY time DESC", $mysql);
-$users_access = mysql_query("SELECT * FROM users WHERE zone_red = 1 and users.deleted = 0", $mysql);
 ?>
 
 
@@ -21,18 +19,8 @@ $users_access = mysql_query("SELECT * FROM users WHERE zone_red = 1 and users.de
 	<td rowspan="2">
 	  <h2 align="center">Данные о зоне:</h2>
 	    <h3 align="center">Информация о сотрудниках</h3>
-	  <p>Имеют право доступа:</p>
-<ul>
-  <?php while ($row_y = mysql_fetch_assoc($users_access)): ?>
-<li><?php echo $row_y['name']; ?></li>
-  <?php endwhile ?>
-  </ul>
+	  <p>Имеют право доступа: <?php echo $user['']; ?></p>
 	  <p>Находятся: <?php echo $user['']; ?></p>
-    <ul>
-  <?php while ($row_y = mysql_fetch_assoc($users)): ?>
-<li><?php echo $row_y['name']; ?></li>
-  <?php endwhile ?>
-    </ul>
 	</td>
   </tr>
   <tr>
@@ -101,6 +89,43 @@ $users_access = mysql_query("SELECT * FROM users WHERE zone_red = 1 and users.de
 	<td width="50%">
 	    <h2 align="center">Изображение зоны:<h2>
 		<img src="images/red3.jpg" style="width:100%" class="img-rounded">
+	</td>
+	<td rowspan="2">
+	  <h2 align="center">Данные о зоне:</h2>
+	    <h3 align="center">Информация о сотрудниках</h3>
+	  <p>Имеют право доступа: <?php echo $user['']; ?></p>
+	  <p>Находятся: <?php echo $user['']; ?></p>
+	</td>
+  </tr>
+  <tr>
+	<td>
+	  <h2 align="center">Аппаратные составляющие:</h2>
+		<ul>
+		<li>Устройство идентификации,двусторонний считыватель</li>
+		<li>Турникет</li>
+		<li>Картоприемник</li>
+		<br>
+		<li>Cчитыватель биометрических данных</li>
+		<br>
+		<li>Шлюзовая кабина</li>
+		<li>Cчитыватель биометрических данных</li>
+	   </ul>
+	</td>
+  </tr>
+  </table>
+</div>
+</html>
+
+<?php endif; ?>
+
+<?php if($area == '4'): ?>
+<div class="hero-unit">
+  <h1 align="center">Красная зона</h1>
+  <table width="100%" border="0">
+  <tr>
+	<td width="50%">
+	    <h2 align="center">Изображение зоны:<h2>
+		<img src="images/red4.jpg" style="width:100%" class="img-rounded">
 	</td>
 	<td rowspan="2">
 	  <h2 align="center">Данные о зоне:</h2>
